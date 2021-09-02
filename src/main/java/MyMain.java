69fb39edff90d6463f15bbf33f56cce064dd18cf
Jack Chen <23hchen@athenian.org>
2021-09-02 09:54:48 -0700

    I wrote Hello World Code.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,6 +1,8 @@
+import java.sql.SQLOutput;
+
 public class MyMain {
     public static void main(String[] args) {
         // Write some code here!
-
+        System.out.println("Hello world!");
     }
 }
